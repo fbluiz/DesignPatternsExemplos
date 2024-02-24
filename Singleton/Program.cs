c0f4d2c4b593c3e47f81e0360ab2cda08ce6cee2
FB <fbluiz.sf@gmail.com>
2024-02-24 19:39:02 -0300

    realizado a implementação do padrão criacional singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using Singleton_Teste;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Iniciando teste da implementação do padrão singleton");
+
+        Parallel.Invoke(
+            () => AcessoThread1(),
+            () => AcessoThread2());
+    }
+
+    private static void AcessoThread1()
+    {
+        Console.WriteLine("Tentando criar a primeira instância");
+
+        Singleton s1 = Singleton.Instance;
+
+    }
+
+    private static void AcessoThread2()
+    {
+        Console.WriteLine("Tentando criar a segunda instância");
+
+        Singleton s2 = Singleton.Instance;
+
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿namespace Singleton_Teste
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private int numeroDeInstancias = 0;
+        private static object lockObject = new object();
+        
+        private Singleton() 
+        {
+            Console.WriteLine("Instanciando dentro do construtor privado");
+            numeroDeInstancias++;
+            Console.WriteLine($"Numero de instancias = {numeroDeInstancias}\n" );
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    //Para permite que o padrão singleton não seja violado em situações multi thread
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            Console.WriteLine("Criando a primeira intancia");
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
